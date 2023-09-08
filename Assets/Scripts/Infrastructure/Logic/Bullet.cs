b9336d925e12c9b29692abd936b7c460d8413f35
Dima34 <vavilov9823@gmail.com>
2023-09-08 23:00:02 +0300

    Ricochey added

--- Assets/Scripts/Infrastructure/Logic/Bullet.cs
+++ Assets/Scripts/Infrastructure/Logic/Bullet.cs
@@ -4,18 +4,28 @@ namespace Infrastructure.Logic
 {
     public class Bullet : MonoBehaviour
     {
+        [SerializeField] private int _maxSpeedDebuffPerc;
+        [SerializeField] private int _minSpeedDebuffPerc;
+        [SerializeField] private int _ricochetAngle;
+        [SerializeField] private int _maxCollisionsCount;
+        
         private float _startTime;
         private Vector3 _startPosition;
+        private Vector3 _prevPos;
         private float _speed;
+        private int _collisionsCount;
 
-        public void Initialize(Vector3 pos, Quaternion rot,float speed)
+        public void Initialize(Vector3 pos, Quaternion rot, float speed)
         {
             transform.position = pos;
             transform.rotation = rot;
             _speed = speed;
         }
 
-        private void Start()
+        private void Start() =>
+            ResetStartTimeAndPosition();
+
+        private void ResetStartTimeAndPosition()
         {
             _startTime = Time.time;
             _startPosition = transform.position;
@@ -23,8 +33,50 @@ namespace Infrastructure.Logic
 
         private void Update()
         {
+            _prevPos = transform.position;
+
             float time = Time.time - _startTime;
-            transform.position = BasicPhysics.BallisticTrajectory(_startPosition,transform.forward, time, _speed); 
+            transform.position = BasicPhysics.BallisticTrajectory(_startPosition, transform.forward, time, _speed);
         }
+
+        private void OnTriggerEnter(Collider collidedObj)
+        {
+            Vector3 flyDirection = GetFlyDirection();
+            Ricochet(collidedObj, flyDirection);
+        }
+
+        private void Ricochet(Collider collidedObj, Vector3 inDirecion)
+        {
+            if (_collisionsCount >= _maxCollisionsCount) 
+                DestroySelf();
+
+            Vector3 collidedObjNormal = collidedObj.transform.up;
+            Vector3 reflectedVector = Vector3.Reflect( -inDirecion, collidedObjNormal);
+
+            ResetStartTimeAndPosition();
+            transform.forward = reflectedVector;
+            
+            float shootAngle = Vector3.Angle(-collidedObj.transform.up, inDirecion);
+            float speedDebuffPercentage = Helpers.LinearTransform(shootAngle, 1, _maxSpeedDebuffPerc, 180, _minSpeedDebuffPerc);
+            _speed = _speed - (_speed / 100 * speedDebuffPercentage);
+            Debug.Log($"Angle {shootAngle}");
+            Debug.Log($"Debuff {speedDebuffPercentage}");
+
+            _collisionsCount++;
+            
+            Debug.DrawRay(transform.position, collidedObjNormal, Color.blue, 5);
+            Debug.DrawRay(transform.position, reflectedVector, Color.green, 5);
+            Debug.DrawRay(transform.position, inDirecion, Color.red, 5);
+            
+        }
+
+        private void DestroySelf() =>
+            Destroy(gameObject);
+
+        private bool IsRicochet(float hitAngle) =>
+            hitAngle > _ricochetAngle;
+
+        private Vector3 GetFlyDirection() =>
+            _prevPos - transform.position;
     }
 }
--- Assets/Scripts/Infrastructure/Logic/Helpers.cs
+++ Assets/Scripts/Infrastructure/Logic/Helpers.cs
@@ -4,5 +4,12 @@ namespace Infrastructure.Logic
     {
         public static float PercentageToLimitedRange(float percentage, float minValue, float maxValue) =>
             (percentage / 100) * (maxValue - minValue) + minValue;
+        
+        public static float LinearTransform(float input, float at, float get, float at1, float get1)
+        {
+            float a = (get1 - get) / (at1 - at);
+            float b = get - a * at;
+            return a * input + b;
+        }
     }
 }
